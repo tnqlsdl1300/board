ef2afae229133a1a0dd23d525aec74d3d8087b9c
SubinPark <hjun34@naver.com>
2021-05-29 20:10:56 +0900

    게시글 수정/삭제/조회 및 테스트 구현

--- src/main/java/com/subin/board/springboot/service/posts/PostsService.java
+++ src/main/java/com/subin/board/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.subin.board.springboot.service.posts;
 
+import com.subin.board.springboot.domain.posts.Posts;
 import com.subin.board.springboot.domain.posts.PostsRepository;
+import com.subin.board.springboot.web.dto.PostsResponseDto;
 import com.subin.board.springboot.web.dto.PostsSaveRequestDto;
+import com.subin.board.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    // 게시글 생성
     @Transactional
     public long save(PostsSaveRequestDto requestDto){
 
@@ -22,4 +26,32 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    // 게시글 수정
+    @Transactional
+    public long update(long id, PostsUpdateRequestDto requestDto) {
+
+        // 1: id로 DB에서 검색을 해봄으로써 DB에 접촉한 entity가 생성됨
+        // -> 트랜잭션 안에서 데이터베이스에서 데이터를 가져오면 이 데이터는 영속성 컨텍스트가 유지된 상태 
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        // 2: 생성된 entity를 가지고 값을 변경 -> 따로 update 쿼리를 날리지 않아도 됨 -> 더티체킹
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+        // 3: 트랜잭션 처리가 끝나면서 변경된 entity가 DB에 반영
+    }
+
+    // 게시글 조회
+    public PostsResponseDto findById(long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+    
+    // 게시글 삭제
+    @Transactional
+    public void deleteById(long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/subin/board/springboot/web/PostsApiController.java
+++ src/main/java/com/subin/board/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.subin.board.springboot.web;
 
 import com.subin.board.springboot.service.posts.PostsService;
+import com.subin.board.springboot.web.dto.PostsResponseDto;
 import com.subin.board.springboot.web.dto.PostsSaveRequestDto;
+import com.subin.board.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,9 +13,28 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 게시글 생성
     @PostMapping("/api/v1/posts")
     public long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    // 게시글 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public long update(@PathVariable long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+    
+    // 게시글 조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable long id){
+        return postsService.findById(id);
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public void deleteById(@PathVariable long id){
+        postsService.deleteById(id);
     }
 
 }
